f3ae9643a3518a3b73181dd31be133c2b758f63a
ninest <parth.kabra@gmail.com>
2020-07-25 18:59:20 +0800

    add SAR 21 animation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,6 +2,7 @@
   <header>
     <nuxt-link class="logo link-hover" to="/">
       <img src="@/assets/svg/sar21.svg" alt="Logo" height="20">
+      <div class="bullet"></div>
     </nuxt-link>
     <nav>
       <ul>
@@ -24,7 +25,32 @@ header {
   justify-content: space-between;
 
   .logo {
+    position: relative;
     display: block;
+
+    &:hover {
+      img { 
+        z-index: 100;
+        animation: shoot 1s infinite; 
+      }
+
+      .bullet { 
+        z-index: 90;
+        animation: bullet 1s infinite; 
+      }
+    }
+
+    .bullet {
+
+      position: absolute;
+      top: 0.60em;
+      left: 4em;
+
+      height: 5px;
+      width: 7px;
+      background: transparent;
+      border-radius: 5px;
+    }
   }
 
   ul {
@@ -44,5 +70,25 @@ header {
       }
     }
   }
+}
+
+@keyframes shoot {
+  0% { transform: rotate(0deg); }
+  10% { transform: rotate(-7deg); }
+  20% { transform: rotate(5deg); }
+  25% { transform: rotate(0deg); }
+  100% { transform: rotate(0deg); }
+}
+
+@keyframes bullet {
+  0% {  }
+  10% { background: linear-gradient(to bottom left, red, orange); }
+  20% {}
+  25% {}
+  50% { 
+    left: 17rem;
+    top: 0.50rem;
+  }
+  100% { background: transparent }
 }
 </style>
