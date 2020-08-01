2caef388c832e280ae0287286376152ea98ca5e4
ninest <parth.kabra@gmail.com>
2020-08-01 13:17:32 +0800

    add error

---
+++ src/layouts/error.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<template>
+  <Default
+    title="Error"
+  >
+    <h2>{{error.statusCode}}</h2>
+    <p v-if="error.statusCode===404">
+      This page was not found, similar to how you cannot find that Pink IC.
+    </p>
+  </Default>
+</template>
